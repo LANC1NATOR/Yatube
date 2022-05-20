4a3278bde1e4438ef6bc9cd3bcac78bc9b660ea2
LANC1NATOR <butovmark0000@gmail.com>
2022-05-20 20:13:55 +0200

    Final

--- yatube/settings.py
+++ yatube/settings.py
@@ -169,13 +169,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'posts/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'posts/static/')]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 INTERNAL_IPS = [
     "127.0.0.1",
